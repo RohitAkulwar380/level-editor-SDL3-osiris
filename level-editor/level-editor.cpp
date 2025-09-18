63a29a3c968164c0c749b6b0606daf6401886f96
pathan25443221 <intekhabpathan125@gmail.com>
2025-09-18 15:25:18 +0530

    SDL Setup

---
+++ level-editor/level-editor.cpp
@@ -0,0 +1,90 @@
+﻿// level-editor.cpp : Defines the entry point for the application.
+//
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
+
+using namespace std;
+
+struct SDLState {
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+};
+
+void cleanup(SDLState &state);
+
+int main(int argc, char *argv[])
+{
+
+	SDLState state;
+
+	
+	if (!SDL_Init(SDL_INIT_VIDEO))
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error Initializing SDL3", nullptr);
+		return 1;
+	}
+
+	//create the window
+	int width = 800;
+	int height = 600;
+	state.window = SDL_CreateWindow("level-editor", width, height, 0);
+	if (!state.window) {
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error creating window",nullptr);
+		cleanup(state);
+		return 1;
+	}
+
+	//create the renderer
+	state.renderer = SDL_CreateRenderer(state.window, nullptr);
+	if (!state.renderer)
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "error", "Error creating Renderer", state.window);
+		cleanup(state);
+		return 1;
+	}
+
+	// load game assets
+	SDL_Texture* idleTex = IMG_LoadTexture(state.renderer, "C:/Users/JIYA/Desktop/level-editor/assets/isometric tileset/separated images/tile_000.png");
+
+	
+	//start the game loop
+	bool running = true;
+	while (running) {
+		SDL_Event event{ 0 };
+		while (SDL_PollEvent(&event))
+		{
+			switch (event.type)
+			{
+			case SDL_EVENT_QUIT:
+			{
+				running = false;
+				break;
+
+			}
+			}
+		}
+
+		//perform drawing commands
+		SDL_SetRenderDrawColor(state.renderer, 255, 255, 255, 255);
+		SDL_RenderClear(state.renderer);
+
+		SDL_RenderTexture(state.renderer, idleTex, nullptr, nullptr);
+
+		//swap buffers and present
+		SDL_RenderPresent(state.renderer);
+
+	}
+
+	SDL_DestroyTexture(idleTex);
+	cleanup(state);
+	return 0;
+}
+
+void cleanup(SDLState &state)
+{
+	SDL_DestroyRenderer(state.renderer);
+	SDL_DestroyWindow(state.window);
+	SDL_Quit();
+}
